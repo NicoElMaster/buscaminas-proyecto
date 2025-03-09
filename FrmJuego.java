d972d6086ed1a320149acfae308c0473742d46bc
Nicolas Moreno Jiménez <102886853+NicoElMaster@users.noreply.github.com>
2025-03-08 20:29:48 -0500

    Update FrmJuego.java

--- FrmJuego.java
+++ FrmJuego.java
@@ -6,15 +6,9 @@ import java.util.function.Consumer;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author BELSOFT
+ * @author Nicolas - Daniel - Sebastian
  */
 public class FrmJuego extends javax.swing.JFrame {
 
